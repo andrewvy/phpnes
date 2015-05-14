e8ae79126bbd0c2d423ad310fe274ea5970b4ef4
Andrew Vy <andrew@andrewvy.com>
2015-05-13 22:35:36 -0700

    Add PPU Tile rendering coroutine

--- src/PHPNES/PPU/Tile.php
+++ src/PHPNES/PPU/Tile.php
@@ -53,7 +53,134 @@ class Tile {
 		}
 	}
 
-	public function render() {
+	public function render($buffer, $srcx1, $srcx2, $srcy2, $dx, $dy, $palAdd, $palette, $flipHorizontal, $flipVertical, $pri, $priTable) {
+		if ($dx < (-7) || $dx >= 256 || $dy < (-7) || $dy >= 240) {
+			return;
+		}
+
+		$this->w = $srcx2 - $srcx1;
+		$this->h = $srcy2 - $srcy1;
+
+		if ($dx < 0) {
+			$srcx1 -= $dx;
+		}
+
+		if ($dx + $srcx2 >= 256) {
+			$srcx2 = 256 - $dx;
+		}
+
+		if ($dy < 0) {
+			$srcy1 -= $dy;
+		}
+
+		if ($dy + $srcy2 >= 240) {
+			$srcy2 = 240 - $dy;
+		}
+
+		if (!$flipHorizontal && !$flipVertical) {
+			$this->fbIndex = ($dy << 8) + $dx;
+			$this->tIndex = 0;
+
+			for ($this->y = 0; $this->y < 8; $this->y++) {
+				for ($this->x = 0; $this->x < 8; $this->x++) {
+					if ($this->x >= $srcx1 && $this->x < $srcx2 && $this->y >= $srcy1 && $this->y < $srcy2) {
+						$this->padIndex = $this->pix[$this->tIndex];
+						$this->tpri = $priTable[$this->fbIndex];
+
+						if ($this->palIndex !== 0 && $pri <= ($this->tpri & 0xFF)) {
+							$buffer[$this->fbIndex] = $palette[$this->palIndex + $palAdd];
+							$this->tpri = ($this->tpri & 0xF00) | pri;
+
+							$priTable[$this->fbIndex] = $this->tpri;
+						}
+					}
+
+					$this->fbIndex++;
+					$this->tIndex++;
+				}
+
+				$this->fbIndex -= 8;
+				$this->fbIndex += 256;
+			}
+		} else if ($flipHorizontal && !$flipVertical) {
+			$this->fbIndex = ($dy << 8) + $dx;
+			$this->tIndex = 7;
+
+			for ($this->y = 0; $this->y < 8; $this->y++) {
+				for ($this->x = 0; $this->x < 8; $this->x++) {
+					if ($this->x >= $srcx1 && $this->x < $srcx2 && $this->y >= $srcy1 && $this->y < $srcy2) {
+						$this->palIndex = $this->pix[$this->tIndex];
+						$this->tpri = $priTable[$this->fbIndex];
+
+						if ($this->palIndex !== 0 && $pri <= ($this->tpri & 0xFF)) {
+							$buffer[$this->fbIndex] = $palette[$this->palIndex + $palAdd];
+							$this->tpri = ($this->tpri & 0xF00) | $pri;
+
+							$priTable[$this->fbIndex] = $this->tpri;
+						}
+					}
+
+					$this->fbIndex++;
+					$this->tIndex--;
+				}
+
+				$this->fbIndex -= 8;
+				$this->fbIndex += 256;
+				$this->tIndex += 16;
+			}
+		} else if ($flipVertical && !$flipHorizontal) {
+			$this->fbIndex = ($dy << 8) + $dx;
+			$this->tIndex = 56;
+
+			for ($this->y = 0; $this->y < 8; $this->y++) {
+				for ($this->x = 0; $this->x < 8; $this->x++) {
+					if ($this->x >= $srcx1 && $this->x < $srcx2 && $this->y >= $srcy1 && $this->y < $srcy2) {
+						$this->palIndex = $this->pix[$this->tIndex];
+						$this->tpri = $priTable[$this->fbIndex];
+
+						if ($this->palIndex !== 0 && $pri <= ($this->tpri & 0xFF)) {
+							$buffer[$this->fbIndex] = $palette[$this->palIndex + $palAdd];
+							$this->tpri = ($this->tpri & 0xF00) | $pri;
+
+							$priTable[$this->fbIndex] = $this->tpri;
+						}
+					}
+
+					$this->fbIndex++;
+					$this->tIndex++;
+				}
+
+				$this->fbIndex -= 8;
+				$this->fbIndex += 256;
+				$this->tIndex -= 16;
+			}
+		} else {
+			$this->fbIndex = ($dy << 8) + $dx;
+			$this->tIndex = 63;
+
+			for ($this->y = 0; $this->y < 8; $this->y++) {
+				for ($this->x = 0; $this->x < 8; $this->x++) {
+					if ($this->x >= $srcx1 && $this->x < $srcx2 && $this->y >= $srcy1 && $this->y < $srcy2) {
+						$this->palIndex = $this->pix[$this->tIndex];
+						$this->tpri = $priTable[$this->fbIndex];
+
+						if ($this->palIndex !== 0 && $pri <= ($this->tpri & 0xFF)) {
+							$buffer[$this->fbIndex] = $palette[$this->palIndex + $palAdd];
+							$this->tpri = ($this->tpri & 0xF00) | $pri;
+
+							$priTable[$this->fbIndex] = $this->tpri;
+						}
+					}
+
+					$this->fbIndex++;
+					$this->tIndex--;
+				}
+
+				$this->fbIndex -= 8;
+				$this->fbIndex += 256;
+			}
+		}
+
 	}
 
 	public function isTransparent($x, $y) {
